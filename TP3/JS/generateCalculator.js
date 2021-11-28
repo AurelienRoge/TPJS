0a4feb2c4844ec2a9d0cf3bc1bba8d9b8fff10de
Leroy-Guillaume <70961560+Leroy-Guillaume@users.noreply.github.com>
2021-11-28 19:38:50 +0100

    yo ptit aure
    je ferai les commentaires tkt

--- TP3/JS/generateCalculator.js
+++ TP3/JS/generateCalculator.js
@@ -1,51 +1,65 @@
-function generateButtons(){
-    
-    var btn0 = document.createElement("BUTTON");
-    btn0.innerHTML = "0";
-    document.body.appendChild(btn0);
-    document.body.appendChild(btn0);
-    var btn1 = document.createElement("BUTTON");
-    btn1.innerHTML = "1";
-    document.body.appendChild(btn1);
-    var btn2 = document.createElement("BUTTON");
-    btn2.innerHTML = "2";
-    document.body.appendChild(btn2);
-    var btn3 = document.createElement("BUTTON");
-    btn3.innerHTML = "3";
-    document.body.appendChild(btn3);
-    var btn4 = document.createElement("BUTTON");
-    btn4.innerHTML = "4";
-    document.body.appendChild(btn4);
-    var btn5 = document.createElement("BUTTON");
-    btn5.innerHTML = "5";
-    document.body.appendChild(btn5);
-    var btn6 = document.createElement("BUTTON");
-    btn6.innerHTML = "6";
-    document.body.appendChild(btn6);
-    var btn7 = document.createElement("BUTTON");
-    btn7.innerHTML = "7";
-    document.body.appendChild(btn7);
-    var btn8 = document.createElement("BUTTON");
-    btn8.innerHTML = "8";
-    document.body.appendChild(btn8);
-    var btn9 = document.createElement("BUTTON");
-    btn9.innerHTML = "9";
-    document.body.appendChild(btn9);
-    var btnPlus = document.createElement("BUTTON");
-    btnPlus.innerHTML = "+";
-    document.body.appendChild(btnPlus);
-    var btnMoins = document.createElement("BUTTON");
-    btnMoins.innerHTML = "-";
-    document.body.appendChild(btnMoins);
-    var btnFois= document.createElement("BUTTON");
-    btnFois.innerHTML = "x";
-    document.body.appendChild(btnFois);
-    var btnDivision = document.createElement("BUTTON");
-    btnDivision.innerHTML = "/";
-    document.body.appendChild(btnDivision);
-    var btnC = document.createElemeent("BUTTON");
-    btnC.innerHTML = "C";
-    document.body.appendChild(btnC);
+var FKeyPad = document.calcform;
+var Accumulate = 0;
+var NewNumber = false;
+var PendingOp = "";
+
+function NumPressed(Num) {
+    if (NewNumber) {
+        FKeyPad.txt1.value = Num;
+        NewNumber = false;
+    }
+    else {
+        if (FKeyPad.txt1.value == "0")
+            FKeyPad.txt1.value = Num;
+        else
+            FKeyPad.txt1.value += Num;
+    }
 }
 
-generateButtons();
+function Operation(Op) {
+    var txt1 = FKeyPad.txt1.value
+    if (NewNumber && PendingOp != "=");
+    else {
+        NewNumber = true;
+        if ('+' == PendingOp)
+            Accumulate += parseFloat(txt1); //parsefloat tranforme les chaines de caractères en nombre 
+        else if ('-' == PendingOp)
+            Accumulate -= parseFloat(txt1);
+        else if ('/' == PendingOp)
+            Accumulate /= parseFloat(txt1);
+        else if ('*' == PendingOp)
+            Accumulate *= parseFloat(txt1);
+        else
+            Accumulate = parseFloat(txt1);
+        FKeyPad.txt1.value = Accumulate;
+        PendingOp = Op;
+    }
+}
+
+function Decimal() {
+    var curtxt1 = FKeyPad.txt1.value;
+    if (NewNumber) {
+        curtxt1 = "0.";
+        NewNumber = false;
+    }
+    else {
+        if (curtxt1.indexOf(".") == -1)
+            curtxt1 += ".";
+    }
+    FKeyPad.txt1.value = curtxt1;
+}
+
+function ClearEntry() {
+    FKeyPad.txt1.value = "0";
+    NewNumber = true;
+} 
+
+function Clear() {
+    Accumulate = 0;
+    PendingOp = "";
+    ClearEntry();
+}
+
+function Neg() {
+    FKeyPad.txt1.value = parseFloat(FKeyPad.txt1.value) * -1;
+}
