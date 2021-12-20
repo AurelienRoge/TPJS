3250fc92ffa70385abc1f1f521259b681d47e5c2
AurelienRoge <aurelien.roge@student.yncrea.fr>
2021-12-21 00:41:31 +0100

    exo1 fix

--- TP4 - Tic Tac Toe - Aurélien.Rogé- Guillaume.Leroy/tp4/exercice_1/Observable.js
+++ TP4 - Tic Tac Toe - Aurélien.Rogé- Guillaume.Leroy/tp4/exercice_1/Observable.js
@@ -1,25 +1,30 @@
-class Observable{
-    constructor(){
+class Observable {
+    constructor() {
         this.tab = new Array();
     }
-    on = function(eventName, callback){
+    on = function (eventName, callback) {
         this.tab.push(new nameWithCallback(eventName, callback))
     }
-    off = function(eventName, callback){
+    off = function (eventName, callback) {
         let indexToRemove = this.tab.findIndex(element => element.name == eventName && element.callback == callback);
-        while(indexToRemove != -1){
+        while (indexToRemove != -1) {
             this.tab.splice(indexToRemove, 1);
             indexToRemove = this.tab.findIndex(element => element.name == eventName && element.callback == callback);
         }
-        
+
     }
-    trigger = function(eventName, callback, ...rest){
-        this.tab.find(element => element.name == eventName).callback(callback, ...rest);
+    trigger = function (eventName, callback, ...rest) {
+        if (this.tab.find(element => element.name == eventName) != undefined) {
+            this.tab.find(element => element.name == eventName).callback(callback, ...rest);
+        }
+        else{
+            console.log("event to trigger not found!");
+        }
     }
 }
 
-class nameWithCallback{
-    constructor(name, callback){
+class nameWithCallback {
+    constructor(name, callback) {
         this.name = name;
         this.callback = callback;
     }
